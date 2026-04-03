6d5ef17a442fc89e927455ffaad8689197fec5c6
IanM <ian@morland.me>
2026-04-03 21:32:00 +0100

    chore(2.0): misc frontend changes
    Miscellaneous frontend changes

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -2,7 +2,7 @@ import app from 'flarum/admin/app';
 import configureRichText from '../common/configureRichText';
 
 app.initializers.add('fof/checklist', () => {
-  app.extensionData.for('fof-checklist').registerSetting({
+  app.registry.for('fof-checklist').registerSetting({
     setting: 'fof-checklist.cross_out_completed_items',
     label: app.translator.trans('fof-checklist.admin.settings.cross_out_completed_items'),
     type: 'boolean',
